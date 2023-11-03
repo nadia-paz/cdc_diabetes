9d74468b4008052540929f68e1274794c68195f6
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-11-03 15:21:00 -0500

    implement transformation

--- deployment/predict.py
+++ deployment/predict.py
@@ -1,12 +1,31 @@
 import requests
 import pickle
 from flask import Flask, request, jsonify
+from pandas import DataFrame
+from numpy import select, concatenate
 
 app = Flask("diabetes")
 
 model_file = "model.bin"
 encoder_file = "encoder.bin"
 transformer_file = "transformer.bin"
+
+binary = ['HighBP', 'HighChol', 'CholCheck',  'Smoker', 'Stroke',
+'HeartDiseaseorAttack', 'PhysActivity', 'HvyAlcoholConsump', 'DiffWalk']
+ordinal = ['MentHlth', 'PhysHlth', 'GenHlth', 'Age', 'Education', 'Income']
+
+bmi = ["BMI_under", "BMI_over"]
+
+def change_values(x, target):
+    ''' 
+    Function to replace values in MentHlth and PhysHlth columns
+    below 10 days -> low, above 20 days -> highbb.;v , everything else "medium"
+    '''
+    # print("inside change values func")
+    # print(f"x: -> {x}, target: -> {target}")
+    conds = [x[target] <= 10, x[target] > 20]
+    choices = ['low', 'high']
+    return select(conds, choices, default='medium').astype("object")
 
 with open(model_file, "rb") as model_in:
     model = pickle.load(model_in)
@@ -16,18 +35,24 @@ with open(encoder_file, "rb") as encoder_in:
     ohe = pickle.load(encoder_in)
 print("OneHotEncoder loaded")
 
-with open(transformer_file, "rb") as transformer_in:
-    Transformer = pickle.load(transformer_in)
-print("Transformer loaded")
-
 #@app.route("/predict")
 @app.route("/predict", methods=["POST"])
 def predict():
     patient = request.get_json()
     
-    t = Transformer(patient, ohe)
+    p = DataFrame([patient])
+    # print("Dataframe created")
+    # display(p)
+    p.MentHlth = change_values(p, 'MentHlth')
+    p.PhysHlth = change_values(p, 'PhysHlth')
+    p["BMI_under"] = p.BMI.map(lambda x: 1 if x <= 18 else 0)
+    p["BMI_over"] = p.BMI.map(lambda x: 1 if x >=25 else 0)
 
-    X = t.transform_single()
+    X = concatenate([
+            p[binary],
+            ohe.transform(p[ordinal]).astype('uint8'),
+            p[bmi].astype('uint8')
+            ], axis=1)
     y = model.predict_proba(X)[:, 1][0].round(2)
     diabetes = y >= 0.4
 
@@ -41,4 +66,4 @@ def predict():
 
 if __name__=="__main__":
     #app.run(debug=True, host='0.0.0.0', port=2912)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=2912)
